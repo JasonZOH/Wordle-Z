3073761169f8fd04f13cc983e942523a2309eeab
JasonZOH <jasonzoh2@gmail.com>
2024-11-24 00:32:09 +0100

    function and test for submit guesses

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
+import useWordle from '../hooks/wordle/useWordle';
 
 const Wordle = () => {
   const solution = useSelector((state) => state.wordData.wordRandomData);
+  const { currentGuess, handleKeyUp } = useWordle(solution);
 
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp])
   return (
     <div>
       <h1>Wordle</h1>
-      {solution && <div>Solution is {solution}</div>}
+      <div>Current guess - {currentGuess}</div>
     </div>
   )
 }
--- src/hooks/wordle/useWordle.jsx
+++ src/hooks/wordle/useWordle.jsx
@@ -8,15 +8,47 @@ const useWordle = ({solution}) => {
   const [iscorrect, setIsCorrect] = useState(false);
 
   const formatGuess = () => {
-    
+    console.log('formatting the guess : ' , currentGuess);
   }
 
   const addNewGuess = () => {
 
   }
 
-  const handleKeyUp = () => {
-    
+  const handleKeyUp = ({ key}) => {
+
+    if(key === 'Enter'){
+      if(turn > 5){
+        console.log('You use all your guesses');
+        return;
+      }
+
+      if(history.includes(currentGuess)){
+        console.log('You already try that word');
+        return;
+      }
+
+      if(currentGuess.length !== 5){
+        console.log("Word must be 5 chars long");
+        return;
+      }
+      formatGuess();
+    }
+
+    if (key === 'Backspace'){
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      })
+      return;
+    }
+
+    if (/^[aA-Za-z]$/.test(key)){
+      if(currentGuess.length < 5){
+        setCurrentGuess((prev) => {
+          return prev + key;
+        })
+      }
+    }
   }
 
 
