a9cb2db922194b7d422355a3456a1d373ee96516
JasonZOH <jasonzoh2@gmail.com>
2024-11-25 22:14:19 +0100

    The game home

---
+++ src/constants/game.jsx
@@ -0,0 +1,14 @@
+
+
+export const game = [
+  {
+    label : 'Wordle',
+    image : "",
+    href: "wordle"
+  },
+  {
+    label: 'Tic-Tac-Toe',
+    image: "",
+    href: "tictactoe"
+  }
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { game } from '../constants/game'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <div className='text-2xl font-bold py-2 border-b-2'>
+        Home
+      </div>
+
+      <div className='grid grid-cols-[repeat(auto-fit, _minmax(100px,_1fr))] gap-6'>
+        {
+          game.map((data, index) => {
+            return (
+              <div key={index} className='mx-auto'>
+                <NavLink
+                  key={data.label}
+                  to={data.href}
+                >
+                  {data.label}
+                </NavLink>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
