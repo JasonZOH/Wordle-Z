2e102564a5a2d7d3e4b7cc89f11fd8f918487bcd
JasonZOH <jasonzoh2@gmail.com>
2024-11-25 13:35:20 +0100

    animation tile

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Row from './Row'
 
-const Grid = ({currentGuesse, guesses, turn}) => {
+const Grid = ({currentGuess, guesses, turn}) => {
   return (
     <div>
       {
         guesses.map((g, index) => {
-          return (
-            <Row key={index} guess={g}/>
-          )
+          if(turn === index){
+            return <Row key={index} currentGuess={currentGuess} />
+          }
+          return <Row key={index} guess={g}/>
         })
       }
     </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
-const Row = ({ guess }) => {
+const Row = ({ guess, currentGuess }) => {
 
   if(guess){
     return (
       <div className='row past'>
         {guess.map((letter, index) => (
           <div key={index} className={letter.color}>{letter.key}</div>
+        ))}
+      </div>
+    )
+  }
+
+  if(currentGuess){
+    const letters = currentGuess.split('');
+    return (
+      <div className='row current'>
+        {
+          letters.map((letter, index) => (
+            <div key={index} className='fill'>{letter}</div>
+          ))
+        }
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     )
